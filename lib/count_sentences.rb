6ca099c794a9bbc4e8d9aa9729aa227637a9a6e5
Steven Hunal <stevehunal@gmail.com>
2019-08-01 16:10:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_end = "."
+   new_array = split(".")
+   new_array.length
   end
 end
